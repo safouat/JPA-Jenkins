c73c90e52122d067ced83fffcab4a93d94ae1ffc
aynuod <dounyagourja2@gmail.com>
2024-09-26 12:01:49 +0100

    change du Jenkinsfile pour l'intégration continue

--- src/main/java/org/jpa/jpa/repository/ModuleRepository.java
+++ src/main/java/org/jpa/jpa/repository/ModuleRepository.java
@@ -1,6 +1,7 @@
 package org.jpa.jpa.repository;
 
 
+import org.jpa.jpa.modele.Module;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ModuleRepository extends CrudRepository<Module, Integer> {
--- src/main/java/org/jpa/jpa/service/ModuleService.java
+++ src/main/java/org/jpa/jpa/service/ModuleService.java
@@ -1,5 +1,6 @@
 package org.jpa.jpa.service;
 
+import org.jpa.jpa.modele.Module;
 import org.jpa.jpa.repository.ModuleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
