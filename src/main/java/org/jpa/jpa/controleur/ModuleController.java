0290b69999c996c1d50badea825cdd9a1f61b48d
aynuod <dounyagourja2@gmail.com>
2024-09-28 20:39:33 +0100

    everything up to date

--- src/main/java/org/jpa/jpa/controleur/ModuleController.java
+++ src/main/java/org/jpa/jpa/controleur/ModuleController.java
@@ -1,2 +1,42 @@
-package org.jpa.jpa.controleur;public class ModuleController {
+package org.jpa.jpa.controleur;
+
+import org.jpa.jpa.modele.Module;
+import org.jpa.jpa.service.ModuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/modules")
+public class ModuleController {
+
+    @Autowired
+    private ModuleService moduleService;
+
+    @GetMapping("/getall")
+    public List<Module> getModules() {
+        return moduleService.getAllModules();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Module> getModule(@PathVariable int id) {
+        return moduleService.getModule(id);
+    }
+
+    @PostMapping("/save")
+    public void ajouterModule(@RequestBody Module module) {
+        moduleService.ajouterModule(module);
+    }
+
+    @PutMapping("/edit/{id}")
+    public void modifierModule(@PathVariable int id, @RequestBody Module module) {
+        moduleService.modifierModule(id, module);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void supprimerModule(@PathVariable int id) {
+        moduleService.supprimerModule(id);
+    }
 }
--- src/main/java/org/jpa/jpa/service/ModuleService.java
+++ src/main/java/org/jpa/jpa/service/ModuleService.java
@@ -5,15 +5,35 @@ import org.jpa.jpa.repository.ModuleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
-
 public class ModuleService {
     @Autowired
     private ModuleRepository moduleRepository;
 
+    public List<Module> getAllModules() {
+        List<Module> modules = new ArrayList<>();
+        moduleRepository.findAll().forEach(modules::add);
+        return modules;
+    }
+
     public void ajouterModule(Module module) {
         moduleRepository.save(module);
+    }
+
+    public Optional<Module> getModule(int id) {
+        return moduleRepository.findById(id);
+    }
+
+    public void supprimerModule(int id) {
+        moduleRepository.deleteById(id);
+    }
+
+    public void modifierModule(int id, Module module) {
+            moduleRepository.save(module);
 
     }
 }
